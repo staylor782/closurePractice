0117f107eae66155aad70002481bf635741ad4d2
Stephen Taylor <staylor782@gmail.com>
2015-10-03 09:33:18 -0600

    Completed - First

--- practice.js
+++ practice.js
@@ -10,11 +10,12 @@ var outer = function(){
 //Invoke outer saving the return value into another variable called 'inner'.
 
   //Code Here
+var inner = outer();
 
 //Once you do that, invoke inner.
 
   //Code Here
-
+inner();
 
 
 //Next problem
@@ -33,7 +34,8 @@ var callFriend = function(){
 //Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
 
   //Code Here
-
+var calling = callFriend();
+console.log(calling("435-215-9248"));
 
 
 //Next Problem
@@ -45,11 +47,19 @@ var callFriend = function(){
 */
 
   //Code Here
+  var makeCounter = function() {
+    var counter = 0;
+    return function() {
+      counter++;
+      return counter;
+    }
+  }
+  
   var count = makeCounter();
-  count() // 1
-  count() // 2
-  count() // 3
-  count() // 4
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -63,7 +73,20 @@ var callFriend = function(){
   Once completed, add a second argument that allows the function to be invoked N number of times.
   After the function has been called N number of times, console.log('STAHHP');
 */
+var fn = function() {
+  // Do Something
+  console.log("Do work.");
+}
 
-
-
-
+var outer1 = function(fn, n) {
+  var counter = 0;
+  return function () {
+    if(counter < n) {
+      fn();
+      counter++;
+    }
+    else {
+      console.log("STAHHP");
+    }
+  }
+}
